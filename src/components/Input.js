79586f53b77c6523c30d2021ad8c785568adffdc
linkqu <hongquanli@qq.com>
2019-08-21 20:09:56 +0800

    //

---
+++ src/components/Input.js
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2009, Linkqu and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Linkqu or the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+/**
+ * Input
+ *
+ * @author hongquanli <hongquanli@qq.com>
+ * @version 1.0 2018-06-29 11:04
+ */
+class Input extends HTMLElement {
+
+    constructor() {
+        super();
+
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        const style = document.createElement('style');
+
+        const type = this.getAttribute("type");
+        const placeholder = this.getAttribute("placeholder");
+
+        // Could also use appendChild().
+        shadowRoot.innerHTML = `
+        <input type="${type ? type : 'text'}" aria-describedby="" ${placeholder ? `placeholder="${placeholder}"` : ''}>
+        `;
+
+        shadowRoot.appendChild(style);
+        style.textContent = `
+            /* :host { } */
+            /* :host-context(h1) { font-style: italic; } */
+            :host { width: 100%; }
+            input {
+                display: block;
+                padding: .125rem .75rem;
+                font-size: 1rem;
+                font-weight: 400;
+                line-height: 1.5;
+                color: #495057;
+                background-color: #fff;
+                background-clip: padding-box;
+                border: 1px solid #ced4da;
+                border-radius: .125rem;
+                transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+            }
+            input:focus {
+                color: #495057;
+                background-color: #fff;
+                border-color: #80bdff;
+                outline: 0;
+                box-shadow: 0 0 0 0.2rem rgba(0,123,255,.25);
+            }`;
+    }
+}
+
+// Define the new element
+window.customElements.define('widget-input', Input);
